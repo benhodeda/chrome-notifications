4bf607d92cf1edd99cee8fe5cecf5ba165d73abe
Ben Hodeda <benhodeda@gmail.com>
2015-11-20 03:08:20 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-module.exports = {
-    notify: notification
-};
+module.exports = NotificationService;
 
 var GCM = require('./gcm');
 
-function notification(app_key, token) {
-    var gcm = new GCM(app_key);
+function NotificationService(app_key){
+    var self = this;
+    self.notify = notification;
+    function notification(token) {
+        var gcm = new GCM(app_key);
 
-    var msg = {
-        registration_ids: [token],
-        time_to_live: 180,
-        data: {
-            message: "Hello mundo cruel :P"
-        }
-    };
+        var msg = {
+            registration_ids: [token],
+            time_to_live: 180,
+            data: {
+                message: "Hello mundo cruel :P"
+            }
+        };
 
-    gcm.send(msg, function(err, response) {
-        console.log(response);
-    });
+        gcm.send(msg, function(err, response) {
+            console.log(response);
+        });
+    }
 }
+
